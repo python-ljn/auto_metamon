b16a81a5766a004e536bac506ec05c4d8699d2e9
dayu <lijianan@cnns.net>
2021-12-23 16:55:18 +0800

    [update] 更新session类

--- metamon/session.py
+++ metamon/session.py
@@ -10,16 +10,8 @@ class Session:
 
     def __init__(self):
         self.session = requests.session()
-        self.base_headers = {
-            'origin': 'https://metamon.radiocaca.com',
-            'referer': 'https://metamon.radiocaca.com/',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                          '(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-        }
 
     def post(self, url, headers: Dict[str, Any], data: Dict[str, Any]):
-        if headers:
-            headers.update(self.base_headers)
         return self.session.post(url, headers=headers, data=data)
 
 
@@ -28,11 +20,17 @@ class MetamonSession(Session):
     def __init__(self, token):
         super(MetamonSession, self).__init__()
         self.token = token
+        self.base_headers = {
+            'origin': 'https://metamon.radiocaca.com',
+            'referer': 'https://metamon.radiocaca.com/',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+            "accesstoken": self.token
+        }
 
     def post(self, url, headers: Dict[str, Any] = None, data: Dict[str, Any] = None, **kwargs):
         if not headers:
-            headers = dict()
-        headers.update(accesstoken=self.token)
+            headers = self.base_headers
         ret = super().post(url, headers, data).json()
         if ret["code"] != "SUCCESS":
             raise ValueError(f"请求失败{ret}")
